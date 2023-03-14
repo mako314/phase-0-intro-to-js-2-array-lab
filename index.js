73270b0cfa57079f0861cac6b158995b0e5f847d
Macolister Bispo <bispo.mac@gmail.com>
2023-03-14 17:39:47 -0400

    commit tree 254f38dc6166e6818b84a9685604ad2c62c6af90

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    let coolCats = [...cats, "Broom"];
+    return coolCats;
+}
+
+function prependCat(){
+    let prepCats = ["Arnold", ...cats];
+    return prepCats;
+}
+
+function removeLastCat(){
+    let sorryLastCat = cats.slice(0, cats.length - 1);
+    return sorryLastCat;
+}
+
+function removeFirstCat(){
+    let sorryFirstCat = cats.slice(1);
+    return sorryFirstCat;
+}
